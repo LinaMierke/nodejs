5be7dd9d6b3d3ea26fb62c70f84a8bbf1a386fa1
lina mierkE <lina_maria89@hotmail.com>
2021-02-11 17:23:47 -0500

    routes and html

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
-
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 //Server
+const data = fs.readFileSync('1-node-farm/starter/dev-data/data.json', "utf-8")
+const dataObject = JSON.parse(data)
+
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathName === "/" || pathName === "/overview") {
         res.end("This is the OVERVIEW");
-    } else if (pathName === '/product') {
-        res.end("this is the product")
+    } else if (pathName === "/product") {
+        res.end("this is the product");
+    } else if (pathName === "/api") {
+        // fs.readFile('1-node-farm/starter/dev-data/data.json', "utf-8", (err, data) => {
+        //     const productData = JSON.parse(data);
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
+
     } else {
         res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header' : 'hello-world'
+            "Content-type": "text/html",
+            "my-own-header": "hello-world",
         });
-        res.end('<h1> page not found </h1>')
+        res.end("<h1> page not found </h1>");
     }
-})
-
-server.listen(8000, '127.0.0.1', () => {
-    console.log("listening on port 8000")
 });
 
-
-
-
-
-
-
-
-
-
-
+server.listen(8000, "127.0.0.1", () => {
+    console.log("listening on port 8000");
+});
 
 // // Blocking, Synchronous way
 
 // const textIn = fs.readFileSync('/Users/linamierke/Github/nodeJS/complete-node-bootcamp/1-node-farm/starter/txt/input.txt', 'utf-8');
 // console.log(textIn);
-
 
 // // const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
 
