60bed3473edf43fb77a14ef43b4d38046175169e
lina mierkE <lina_maria89@hotmail.com>
2021-02-13 17:23:48 -0500

    adding routes

--- index.js
+++ index.js
@@ -3,23 +3,62 @@ const http = require("http");
 const url = require("url");
 
 //Server
-const data = fs.readFileSync('1-node-farm/starter/dev-data/data.json', "utf-8")
-const dataObject = JSON.parse(data)
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace('{%PRODUCTNAME%}', product.productName);
+    output = output.replace('{%IMAGE%}', product.image);
+    output = output.replace('{%PRICE%}', product.price);
+    output = output.replace('{%FROM%}', product.from);
+    output = output.replace('{%NUTRIENTS%}', product.nutrients);
+    output = output.replace('{%QUANTITY%}', product.quantity);
+    output = output.replace('{%DESCRIPTION%}', product.description);
+    output = output.replace('{%ID%}', product.id);
 
+    if (!product.organic)
+        output = output.replace('{%NOT_ORGANIC%}', "not-organic");
+    return output;
+};
+const tempProduct = fs.readFileSync(
+    "1-node-farm/starter/templates/template-product.html",
+    "utf-8"
+);
+const tempCard = fs.readFileSync(
+    "1-node-farm/starter/templates/template-card.html",
+    "utf-8"
+);
+const tempOverview = fs.readFileSync(
+    "1-node-farm/starter/templates/template-overview.html",
+    "utf-8"
+);
+
+const data = fs.readFileSync("1-node-farm/starter/dev-data/data.json", "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
 
-    if (pathName === "/" || pathName === "/overview") {
-        res.end("This is the OVERVIEW");
-    } else if (pathName === "/product") {
-        res.end("this is the product");
-    } else if (pathName === "/api") {
+    const {query, pathname } = url.parse(req.url, true);
+
+    //Overview page
+    if (pathname === "/" || pathname === "/overview") {
+        res.writeHead(200, { "Content-type": "text/html" });
+
+        const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        // console.log(cardsHtml);
+        res.end(output);
+
+        //Product page
+    } else if (pathname === "/product") {
+        console.log(query);
+        res.end("This is the product");
+
+        //Api page
+    } else if (pathname === "/api") {
         // fs.readFile('1-node-farm/starter/dev-data/data.json', "utf-8", (err, data) => {
         //     const productData = JSON.parse(data);
-        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.writeHead(200, { "Content-type": "application/json" });
         res.end(data);
 
+        //Not found
     } else {
         res.writeHead(404, {
             "Content-type": "text/html",
