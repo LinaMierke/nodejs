d2f7d0f87bc1244460d8804d39b2d5cf85b8f0b8
lina mierkE <lina_maria89@hotmail.com>
2021-02-16 18:02:23 -0500

    slugify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require('slugify');
 const replaceTemplate = require("./modules/replaceTemplate")
 // const { URL } = require('url');
 // const { pathname, searchParams } = new URL(req.url, 'http://127.0.0.1/');
@@ -22,6 +23,8 @@ const tempOverview = fs.readFileSync(
 
 const data = fs.readFileSync("1-node-farm/starter/dev-data/data.json", "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
